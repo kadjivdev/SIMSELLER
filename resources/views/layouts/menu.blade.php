fdabe1830ce87607b480ab6c98ec007a0315c5eb
kadjivdev <kadjivdev@gmail.com>
2025-04-03 17:02:27 +0100

    rajout du logo AdminLTELog.png

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4 border-bottom-0 sidebar-mini">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link bg-gradient-green">
-        <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{asset('AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-bolder text-white" style="font-size: 18px">Cim Seller</span>
     </a>
 
@@ -12,7 +12,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{asset('AdminLTELogo.png')}}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ Auth::user()?Auth::user()->name:"--" }}</a>
