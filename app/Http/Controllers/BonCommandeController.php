7bd37e5d907f87be8c358537d3fba8901c926b56
kadjivdev <kadjivdev@gmail.com>
2025-04-04 18:02:27 +0100

    revu 2 du BonCommandeController

--- app/Http/Controllers/BonCommandeController.php
+++ app/Http/Controllers/BonCommandeController.php
@@ -32,7 +32,7 @@ class BonCommandeController extends Controller
     public function __construct(TypeCommande $typecommandes, Fournisseur $fournisseurs, Produit $produits)
     {
         // $this->middleware('superviseur')->only(['valider', 'invalider', 'retournerCommande']);
-        $this->middleware('gest')->only(['create', 'delete', 'envoyerCommande']);
+        // $this->middleware('gest')->only(['create', 'delete', 'envoyerCommande']);
 
         $this->typecommandes = $typecommandes;
         $this->fournisseurs = $fournisseurs;
